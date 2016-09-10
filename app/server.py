7392ba03173f7774d580accc1d2b9ed7fccc515c
Ryosuke Kamei <sr2smail@gmail.com>
2016-09-10 16:41:38 +0900

    いろいろ修正

--- app/server.py
+++ app/server.py
@@ -1,27 +1,63 @@
-from bottle import route, run
+# bottleのライブラリ
+from bottle import route, run, request
+
 # MySQLドライバはmysql.connector
 import mysql.connector
+
+# 補足
+# 本当はテンプレートを入れるとHTMLが綺麗になります。
+# その辺は後日…
+
 # hostのIPアドレスは、$ docker inspect {データベースのコンテナ名}で調べる
 # MySQLのユーザやパスワード、データベースはdocker-compose.ymlで設定したもの
-connector = mysql.connector.connect(
-            user='python',
-            password='python',
-            host='172.17.0.3',
-            database='sample')
+# user     : MYSQL_USER
+# password : MYSQL_PASSWORD
+# database : MYSQL_DATABASE
+connector = mysql.connector.connect (
+            user     = 'bottle',
+            password = 'bottle',
+            host     = '172.17.0.3',
+            database = 'measurement'
+)
 
-cursor = connector.cursor()
-cursor.execute("select * from users")
 
-disp = ""
-for row in cursor.fetchall():
-    disp = "ID:" + str(row[0]) + "  名前:" + row[1]
-
-cursor.close
-connector.close
 			
-@route('/hello')
-def hello():
+@route('/list')
+def list():
+    # 温度を表示
+    cursor = connector.cursor()
+    cursor.execute("select `id`, `temperature`, `careted_at` from temperatures")
+
+    disp  = "<table>"
+    # ヘッダー
+    disp += "<tr><th>ID</th><th>温度</th><th>登録日</th></tr>"
+    
+    # 一覧部分
+    for row in cursor.fetchall():
+        disp += "<tr><td>" + str(row[0]) + "</td><td>" + str(row[1]) + "</td><td>" + str(row[2]) + "</td></tr>"
+    
+    disp += "</table>"
+    
+    cursor.close
 
     return "DBから取得 "+disp
 
+@route('/input_temperature')
+def input_temperature():
+    # 温度を入力
+    cursor = connector.cursor()
+    cursor.execute("INSERT INTO `temperatures` (`server_id`, `temperature`, `careted_at`, `careted_user`, `updated_at`, `updated_user`) VALUES (" + request.query.server_id + ", " + request.query.temperature + ", NOW(), " + request.query.user_id + ", NOW(), " + request.query.user_id + ")
+
+    # コミット
+    connector.commit();
+
+    cursor.close
+
+    return true
+    
+
+# コネクターをクローズ
+connector.close
+
+# サーバ起動
 run(host='0.0.0.0', port=8080, debug=True, reloader=True)
--- initdb.d/create_table.sql
+++ initdb.d/create_table.sql
@@ -1,9 +1,17 @@
-USE sample;
+USE measurement;
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+CREATE TABLE `temperatures` (
+  `id`           int(11) NOT NULL AUTO_INCREMENT,
+  `server_id`    int(11) NOT NULL,
+  `temperature`  double(3, 10) NOT NULL,
+  `careted_at`   int(14) NOT NULL,
+  `careted_user` int(11) NOT NULL,
+  `updated_at`   int(14) NOT NULL,
+  `updated_user` int(11) NOT NULL,
   KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `users` (`id`, `name`) VALUES (1, 'pyhons');
+INSERT INTO `temperatures` 
+  (`id`, `server_id`, `temperature`, `careted_at`, `careted_user`, `updated_at`, `updated_user`) 
+  VALUES 
+  (1, 1, 29.123456789, 20160101001122, 1, 20160101001122, 1);
