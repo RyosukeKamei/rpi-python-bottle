3ff4c906ab553ac0c7cd37e3429ec8a9a7efdc28
Ryosuke Kamei <sr2smail@gmail.com>
2016-09-10 22:05:32 +0900

    SQL修正

--- initdb.d/create_table.sql
+++ initdb.d/create_table.sql
@@ -3,10 +3,10 @@ USE measurement;
 CREATE TABLE `temperatures` (
   `id`           int(11) NOT NULL AUTO_INCREMENT,
   `server_id`    int(11) NOT NULL,
-  `temperature`  double(3, 10) NOT NULL,
-  `careted_at`   int(14) NOT NULL,
+  `temperature`  int(11) NOT NULL,
+  `careted_at`   datetime NOT NULL,
   `careted_user` int(11) NOT NULL,
-  `updated_at`   int(14) NOT NULL,
+  `updated_at`   datetime NOT NULL,
   `updated_user` int(11) NOT NULL,
   KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -14,4 +14,4 @@ CREATE TABLE `temperatures` (
 INSERT INTO `temperatures` 
   (`id`, `server_id`, `temperature`, `careted_at`, `careted_user`, `updated_at`, `updated_user`) 
   VALUES 
-  (1, 1, 29.123456789, 20160101001122, 1, 20160101001122, 1);
+  (1, 1, 29, NOW(), 1, NOW(), 1);
